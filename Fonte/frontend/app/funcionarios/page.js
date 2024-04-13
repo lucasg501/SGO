922a7f062cb9e6382992be424e03d54c492b3d44
alexandreSilva604 <xandlibra604@gmail.com>
2024-04-13 14:42:59 -0300

    Incluída a listagem de funcionários

--- Fonte/frontend/app/funcionarios/page.js
+++ Fonte/frontend/app/funcionarios/page.js
@@ -1,6 +1,26 @@
 'use client'
 
+import { useEffect, useState } from "react"
+import httpClient from "../utils/httpClient";
+
 export default function funcionarios() {
+
+    const [listaFuncionarios, setListaFuncionarios] = useState([]);
+
+    function carregarFuncionarios() {
+
+        httpClient.get('/funcionarios/listar')
+        .then(r => {
+            return r.json();
+        })
+        .then(r => {
+            setListaFuncionarios(r);
+        });
+    }
+
+    useEffect(() => {
+        carregarFuncionarios();
+    })
 
     return (
         <div>
@@ -9,7 +29,27 @@ export default function funcionarios() {
 
             <div style={{marginTop: 30}} className="table-responsive">
                 <table className="table table-hover">
-                    
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Telefone</th>
+                            <th>Cargo</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            listaFuncionarios.map((funcionario, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{funcionario.nomeFuncionario}</td>
+                                        <td>{funcionario.telFuncionario}</td>
+                                        <td>{funcionario.cargoFuncionario}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
                 </table>
             </div>
         </div>
