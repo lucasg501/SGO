c12763c6a8bf600d086a101ff909acf4c5953c81
lucasg501 <56370356+lucasg501@users.noreply.github.com>
2024-04-16 13:23:27 -0300

    DataFim marcada com sucesso porem ainda com erro em mostrar as datas anteriores na hora de marcar que a etapa foi finalizada

--- Fonte/frontend/app/components/formEtapas.js
+++ Fonte/frontend/app/components/formEtapas.js
@@ -28,7 +28,7 @@ export default function FormEtapas(props) {
             idObra: prevState.idObra || (listaObras.length > 0 ? listaObras[0].idObra : 0) // Definindo um valor padrão para idObra
         }));
     };
-    
+
 
     const removerCampo = () => {
         if (etapas.idEtapa > 0) {
@@ -55,21 +55,21 @@ export default function FormEtapas(props) {
             });
     }
 
-    function gravarEtapas(){
+    function gravarEtapas() {
         let status = 0;
         let etapasArray = [];
-    
+
         // Verifique se o idObra do primeiro elemento não é undefined
         const idObraValue = idObra.current[0] ? idObra.current[0].value : null;
-    
+
         // Use etapas.length para percorrer todos os elementos presentes em etapas
-        for(let i = 0; i < etapas.idEtapa + 1; i++){
+        for (let i = 0; i < etapas.idEtapa + 1; i++) {
             const idEtapaValue = idEtapa.current[i] ? idEtapa.current[i].value : null;
             const dataPrevInicioValue = dataPrevInicio.current[i] ? dataPrevInicio.current[i].value : null;
             const dataPrevTerminoValue = dataPrevTermino.current[i] ? dataPrevTermino.current[i].value : null;
             const dataFimValue = dataFim.current[i] ? (dataFim.current[i].value !== '' ? dataFim.current[i].value : null) : null;
             const descricaoEtapaValue = descricaoEtapa.current[i] ? descricaoEtapa.current[i].value : null;
-            
+
             const etapa = {
                 idObra: idObraValue, // Use o idObra do primeiro elemento
                 idEtapa: idEtapaValue,
@@ -80,7 +80,7 @@ export default function FormEtapas(props) {
             };
             etapasArray.push(etapa);
         }
-    
+
         httpClient.post('/andamentoEtapas/gravar', etapasArray)
             .then(r => {
                 status = r.status;
@@ -88,20 +88,21 @@ export default function FormEtapas(props) {
             })
             .then(r => {
                 alert(r.msg);
-                if(status === 200){
+                if (status === 200) {
                     window.location.reload();
                 }
             });
     }
 
-    function alterarEtapas(){
+    function alterarEtapas() {
         let status = 0;
         httpClient.put('/andamentoEtapas/alterar',{
+            idAndamento: props.etapa.idAndamento,
             idObra: props.etapa.idObra,
             idEtapa: props.etapa.idEtapa,
-            dataPrevInicio: props.etapa.dataPrevInicio,
-            dataPrevTermino: props.etapa.dataPrevTermino,
-            dataFim: dataFim.current[i].value,
+            dataPrevInicio: formatarData(props.etapa.dataPrevInicio),
+            dataPrevTermino: formatarData(props.etapa.dataPrevTermino),
+            dataFim: formatarData(dataFim.current[0].value),
             descricaoEtapa: props.etapa.descricaoEtapa
         })
         .then(r=>{
@@ -114,23 +115,31 @@ export default function FormEtapas(props) {
                 window.location.reload();
             }
         })
+        
     }
-    
-    
+
+    function formatarData(data) {
+        const dataObj = new Date(data);
+        const ano = dataObj.getFullYear();
+        const mes = ('0' + (dataObj.getMonth() + 1)).slice(-2);
+        const dia = ('0' + dataObj.getDate()).slice(-2);
+        return `${ano}/${mes}/${dia}`;
+    }
+
     return (
         <div>
-            <h1>Gerenciar Etapas da Obra</h1>
+            <h1>{props.etapa != null ? 'Marcar Como Finalizada' : 'Gerenciar Etapas da Obra'}</h1>
 
             <div>
                 <div className="from-group">
                     <label>Obra:</label>
-                    <select ref={el => idObra.current[0] = el} style={{ width: '10%', textAlign: 'center' }} defaultValue={props.etapa.idObra} className="form-control">
+                    <select ref={el => idObra.current[0] = el} style={{ width: '10%', textAlign: 'center' }} defaultValue={props.etapa.idObra} className="form-control" disabled={props.etapa !== null}>
                         <option value={0}>Selecione</option>
                         {
-                            listaObras.map(function(value,index){
-                                if(value.idObra === props.etapa.idObra){
+                            listaObras.map(function (value, index) {
+                                if (value.idObra === props.etapa.idObra) {
                                     return <option key={index} value={value.idObra} selected>{value.bairro}</option>
-                                }else{
+                                } else {
                                     return <option key={index} value={value.idObra}>{value.bairro}</option>
                                 }
                             })
@@ -138,18 +147,17 @@ export default function FormEtapas(props) {
                     </select>
                 </div>
                 <br></br>
-
                 {[...Array(etapas.idEtapa + 1)].map((_, index) => (
                     <div key={index}>
                         <div className="form-group">
                             <label>Etapa {index + 1}:</label>
-                            <select defaultValue={props.etapa.idEtapa} ref={el => idEtapa.current[index] = el} style={{ width: '10%', textAlign: 'center' }} className="form-control">
+                            <select defaultValue={props.etapa.idEtapa} ref={el => idEtapa.current[index] = el} style={{ width: '10%', textAlign: 'center' }} className="form-control" disabled={props.etapa !== null}>
                                 <option value={0}>Selecione</option>
                                 {
-                                    listaEtapas.map(function(value,index){
-                                        if(value.idEtapa === props.etapa.idEtapa){
+                                    listaEtapas.map(function (value, index) {
+                                        if (value.idEtapa === props.etapa.idEtapa) {
                                             return <option key={index} value={value.idEtapa} selected>{value.nomeEtapa}</option>
-                                        }else{
+                                        } else {
                                             return <option key={index} value={value.idEtapa}>{value.nomeEtapa}</option>
                                         }
                                     })
@@ -159,33 +167,34 @@ export default function FormEtapas(props) {
 
                         <div className="form-group" style={{ display: 'inline-block', width: '15%', marginRight: '10px' }}>
                             <label>Início:</label>
-                            <input defaultValue={props.etapa ? props.etapa.dataPrevInicio : ''} ref={el => dataPrevInicio.current[index] = el} style={{ width: '80%' }} type="date" className="form-control" placeholder="Início" />
+                            <input defaultValue={props.etapa ? formatarData(props.etapa.dataPrevInicio) : ''} ref={el => dataPrevInicio.current[index] = el} style={{ width: '80%' }} type="date" className="form-control" placeholder="Início" disabled={props.etapa !== null} />
                         </div>
                         <div className="form-group" style={{ display: 'inline-block', width: '25%' }}>
                             <label>Previsão de Término:</label>
-                            <input defaultValue={props.etapa ? props.etapa.dataPrevTermino : ''} ref={el => dataPrevTermino.current[index] = el} style={{ width: '40%' }} type="date" className="form-control" placeholder="Previsão de Término" />
+                            <input defaultValue={props.etapa ? formatarData(props.etapa.dataPrevTermino) : ''} ref={el => dataPrevTermino.current[index] = el} style={{ width: '40%' }} type="date" className="form-control" placeholder="Previsão de Término" disabled={props.etapa !== null} />
                         </div>
 
                         <div style={{ display: 'flex', alignItems: 'baseline' }}>
                             <div className="form-group" style={{ width: '15%', marginRight: '10px' }}>
                                 <label style={{ display: 'block' }}>Fim:</label>
-                                <input defaultValue={props.etapa ? props.etapa.dataFim : ''} ref={el => dataFim.current[index] = el} style={{ width: '80%' }} type="date" className="form-control" placeholder="Fim" />
+                                <input defaultValue={props.etapa ? formatarData(props.etapa.dataFim) : ''} ref={el => dataFim.current[index] = el} style={{ width: '80%' }} type="date" className="form-control" placeholder="Fim" />
                             </div>
                             <div className="form-group" style={{ width: '25%' }}>
                                 <label style={{ display: 'block' }}>Descrição:</label>
-                                <textarea defaultValue={props.etapa.descricaoEtapa} ref={el => descricaoEtapa.current[index] = el} style={{ width: '100%', minHeight: '100px' }} className="form-control" placeholder="Descrição" />
+                                <textarea defaultValue={props.etapa.descricaoEtapa} ref={el => descricaoEtapa.current[index] = el} style={{ width: '100%', minHeight: '100px' }} className="form-control" placeholder="Descrição" disabled={props.etapa !== null} />
                             </div>
                         </div>
                     </div>
                 ))}
 
+
                 <div className="form-group">
                     <div style={{ display: 'inline-block' }}>
-                        <button className="btn btn-danger" onClick={removerCampo}>-</button>
+                        <button className="btn btn-danger" onClick={removerCampo} disabled={props.etapa !== null}>-</button>
                     </div>
 
                     <div style={{ display: 'inline-block', marginLeft: 15 }}>
-                        <button className="btn btn-primary" onClick={adicionarCampo}>+</button>
+                        <button className="btn btn-primary" onClick={adicionarCampo} disabled={props.etapa !== null}>+</button>
                     </div>
                 </div>
             </div>
@@ -194,7 +203,7 @@ export default function FormEtapas(props) {
 
             <div>
                 <Link style={{ marginRight: 25 }} href="/etapas"><button className="btn btn-secondary">Voltar</button></Link>
-                <button className="btn btn-primary" onClick={gravarEtapas}>Gravar</button>
+                <button className="btn btn-primary" onClick={props.etapa == 0 ? gravarEtapas : alterarEtapas}>Gravar</button>
             </div>
         </div>
     );
