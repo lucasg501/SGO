d81ee83bba1e4ce35b70636d026ee11b144a274d
alexandreSilva604 <xandlibra604@gmail.com>
2024-04-26 11:45:14 -0300

    Primeira versão da seleção de dias feita. Exibição do total ainda não sincroniza com a seleção.

--- Fonte/frontend/app/funcionarios/diarias/[idFuncionario]/page.js
+++ Fonte/frontend/app/funcionarios/diarias/[idFuncionario]/page.js
@@ -2,14 +2,25 @@
 
 import httpClient from "@/app/utils/httpClient";
 import { useEffect, useRef, useState } from "react";
-import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
+import { Calendar } from "react-multi-date-picker";
+import gregorian_pt_br from "react-date-object/locales/gregorian_pt_br";
 
 export default function GerenciarDiarias({params: {idFuncionario}}) {
 
     const [funcionario, setFuncionario] = useState(null);
     const [cargo, setCargo] = useState(null);
+    const [dias, setDias] = useState([]);
+    const [totalDiarias, setTotalDiarias] = useState(0);
     const valorDiarias = useRef(0);
+
+    const formatarData = (data) => {
+        const dataObj = new Date(data);
+        const ano = dataObj.getUTCFullYear();
+        const mes = ('0' + (dataObj.getUTCMonth() + 1)).slice(-2);
+        const dia = ('0' + dataObj.getUTCDate()).slice(-2);
+        return `${ano}-${mes}-${dia}`;
+    };
 
     function carregarFuncionario() {
 
@@ -34,6 +45,19 @@ export default function GerenciarDiarias({params: {idFuncionario}}) {
         });
     }
 
+    function mudarDias(diasSelecionados) {
+
+        let diasTransformados = [];
+
+        diasSelecionados.map((dia, index) => {
+            diasTransformados.push(new Date(dia.year, dia.month, dia.day));
+            diasTransformados[index] = formatarData(diasTransformados[index]);
+        })
+
+        setDias(diasTransformados);
+        setTotalDiarias(valorDiarias.current.value * dias.length);
+    }
+
     useEffect(() => {
         carregarFuncionario();
     }, [])
@@ -54,13 +78,17 @@ export default function GerenciarDiarias({params: {idFuncionario}}) {
                     <div className="input-group" style={{margin: 20, display: "flex", alignItems: "center"}}>
                         <label><b>Valor da diária:</b> R$ </label>
                         <div style={{marginLeft: 10, width: 80}}>
-                            <input type="number" className="form-control" />
+                            <input type="number" className="form-control" ref={valorDiarias} 
+                            onChange={(e) => {setTotalDiarias(e.target.value * dias.length)}} />
                         </div>
                     </div> { /* Será mudado, isto é para testar a interface */ }
-                    <div style={{marginLeft: 20}}><b>Total: R$ 10</b></div>
+                    
+                    <div style={{marginLeft: 20}}><b>Total: R$ {totalDiarias}</b></div>
+                    
                     <div style={{margin: 20}}>
-                        <Calendar />
+                        <Calendar locale={gregorian_pt_br} multiple onChange={(selecao) => mudarDias(selecao)} />
                     </div>
+                    
                     <div style={{margin: 20, marginTop: 40}}>
                         <button className="btn btn-primary">Gravar</button>
                         <a href="/funcionarios" style={{marginLeft: 40}}><button className="btn btn-secondary">Voltar</button></a>
