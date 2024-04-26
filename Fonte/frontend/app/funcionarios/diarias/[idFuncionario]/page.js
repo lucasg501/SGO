3c599f5e4713bf483bf1e0bc69b599e9199c829d
alexandreSilva604 <xandlibra604@gmail.com>
2024-04-26 10:26:50 -0300

    Interface da página de diárias pronta, funcionamento em progresso

--- Fonte/frontend/app/funcionarios/diarias/[idFuncionario]/page.js
+++ Fonte/frontend/app/funcionarios/diarias/[idFuncionario]/page.js
@@ -1,12 +1,15 @@
 'use client'
 
 import httpClient from "@/app/utils/httpClient";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 
 export default function GerenciarDiarias({params: {idFuncionario}}) {
 
     const [funcionario, setFuncionario] = useState(null);
     const [cargo, setCargo] = useState(null);
+    const valorDiarias = useRef(0);
 
     function carregarFuncionario() {
 
@@ -45,7 +48,22 @@ export default function GerenciarDiarias({params: {idFuncionario}}) {
                     <div className="card" style={{padding: 20}}>
                         <h3><b>Nome:</b> {funcionario.nomeFuncionario}</h3>
                         <div><b>Telefone:</b> {funcionario.telFuncionario}</div>
-                        <div><b>Cargo</b>: {cargo.nomeCargo}</div>
+                        <div><b>Cargo:</b> {cargo.nomeCargo}</div>
+                    </div>
+
+                    <div className="input-group" style={{margin: 20, display: "flex", alignItems: "center"}}>
+                        <label><b>Valor da diária:</b> R$ </label>
+                        <div style={{marginLeft: 10, width: 80}}>
+                            <input type="number" className="form-control" />
+                        </div>
+                    </div> { /* Será mudado, isto é para testar a interface */ }
+                    <div style={{marginLeft: 20}}><b>Total: R$ 10</b></div>
+                    <div style={{margin: 20}}>
+                        <Calendar />
+                    </div>
+                    <div style={{margin: 20, marginTop: 40}}>
+                        <button className="btn btn-primary">Gravar</button>
+                        <a href="/funcionarios" style={{marginLeft: 40}}><button className="btn btn-secondary">Voltar</button></a>
                     </div>
                 </div>
                 :
