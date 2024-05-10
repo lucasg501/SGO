b46ddbed48a1f6da91b95f9a45edb2bb97cd3f7b
alexandreSilva604 <xandlibra604@gmail.com>
2024-05-10 09:57:49 -0300

    Filtros do relatório de diárias implementados

--- Fonte/frontend/app/funcionarios/diarias/page.js
+++ Fonte/frontend/app/funcionarios/diarias/page.js
@@ -8,6 +8,8 @@ export default function Diarias() {
 
     const [listaDiarias, setListaDiarias] = useState([]);
     const [listaFuncionarios, setListaFuncionarios] = useState([]);
+    const mostrarSomentePagas = useRef(false);
+    const mesFiltroDiarias = useRef(0);
     const idFuncDiarias = useRef(0);
 
     function formatarData(data) {
@@ -34,7 +36,22 @@ export default function Diarias() {
                 return r.json();
             })
             .then(r => {
-                setListaDiarias(r);
+                let listaCarregar = [];
+
+                r.forEach((diaria) => {
+
+                    let dataDiaria = new Date(diaria.dia);
+                    let mesDiaria = (dataDiaria.getUTCMonth() + 1).toString();
+
+                    if (mesFiltroDiarias.current.value == "0" || mesDiaria === mesFiltroDiarias.current.value) {
+
+                        if (!mostrarSomentePagas.current.checked || diaria.dataPgto != null) {
+                            listaCarregar.push(diaria);
+                        }
+                    }
+                });
+
+                setListaDiarias(listaCarregar);
             });
         }
         else {
@@ -66,83 +83,115 @@ export default function Diarias() {
         <div>
             <h1>Relatório de Diárias</h1>
 
-            <div style={{width: 250, marginTop: 20, marginBottom: 20}}>
+            <div style={{marginTop: 20, marginBottom: 20}}>
                 <label style={{fontWeight: "bold"}}>Selecione um funcionário</label>
-                <select className="form-control" onChange={(e) => carregarDiarias(e.target.value)} ref={idFuncDiarias}>
+                <select className="form-select" style={{width: 250}}
+                    onChange={(e) => carregarDiarias(e.target.value)} ref={idFuncDiarias}>
                     <option value={0}>Nenhum</option>
                     {
                         listaFuncionarios.map((funcionario) => {
-                            return <option value={funcionario.idFuncionario}>{funcionario.nomeFuncionario}</option>
+                            return <option value={funcionario.idFuncionario} key={funcionario.idFuncionario}>
+                                    {funcionario.nomeFuncionario}
+                                </option>
                         })
                     }
                 </select>
             </div>
+            {
+                idFuncDiarias.current.value > 0 ?
+                    <div className="card" style={{padding: 20}}>
+                        <p style={{fontWeight: 'bold'}}>Filtros</p>
+                        <div style={{marginBottom: 20, display: 'inline-flex', alignItems: 'center'}}>
+                            
+                            <div className="form-group">
+                                <label style={{fontWeight: 'bold'}}>Mês</label>
+                                <select className="form-select" style={{width: 250}} onChange={(e) => carregarDiarias(idFuncDiarias.current.value)} 
+                                    ref={mesFiltroDiarias}>
+                                    <option value={0}>Todos</option>
+                                    <option value={1}>Janeiro</option>
+                                    <option value={2}>Fevereiro</option>
+                                    <option value={3}>Março</option>
+                                    <option value={4}>Abril</option>
+                                    <option value={5}>Maio</option>
+                                    <option value={6}>Junho</option>
+                                    <option value={7}>Julho</option>
+                                    <option value={8}>Agosto</option>
+                                    <option value={9}>Setembro</option>
+                                    <option value={10}>Outubro</option>
+                                    <option value={11}>Novembro</option>
+                                    <option value={12}>Dezembro</option>
+                                </select>
+                            </div>
+                            <div className="form-check" style={{marginLeft: 10}}>
+                                <input type="checkbox" onChange={carregarDiarias(idFuncDiarias.current.value)}
+                                    ref={mostrarSomentePagas} />
+                                <label style={{fontWeight: 'bold', marginLeft: 3}}>Mostrar somente diárias pagas</label>
+                            </div>
+                        </div>
 
-            <div>
-                {
-                    idFuncDiarias.current.value > 0 ?
-                        listaDiarias.length > 0 ?
-                        <table style={{textAlign: "center"}} className="table table-responsive">
-                            <thead>
-                                <th>Dia</th>
-                                <th>Valor</th>
-                                <th>Data de Pagamento</th>
-                                <th>Marcar como Paga</th>
-                                <th>Cancelar Pagamento</th>
-                            </thead>
+                        {
+                            listaDiarias.length > 0 ?
+                            <table style={{textAlign: "center"}} className="table table-responsive">
+                                <thead>
+                                    <th>Dia</th>
+                                    <th>Valor</th>
+                                    <th>Data de Pagamento</th>
+                                    <th>Marcar como Paga</th>
+                                    <th>Cancelar Pagamento</th>
+                                </thead>
 
-                            <tbody>
-                                {
-                                    
-                                    listaDiarias.map((diaria) => {
-                                        return (
-                                            <tr key={diaria.idDiaria}>
-                                                <td>{formatarData(diaria.dia)}</td>
-                                                <td>R$ {parseFloat(diaria.valorDiaria).toFixed(2).replace('.', ',')}</td>
-                                                <td>
-                                                    {
-                                                        diaria.dataPgto ?
-                                                        formatarData(diaria.dataPgto)
-                                                        :
-                                                        "Não foi paga"
-                                                    }
-                                                </td>
-                                                <td>
-                                                    {
-                                                        diaria.dataPgto ?
-                                                        <button disabled className="btn btn-success" href={`diarias/alterar/${diaria.idDiaria}`}>
-                                                            <i className="fas fa-check"></i>
-                                                        </button>
-                                                        :
-                                                        <Link className="btn btn-success" href={`diarias/alterar/${diaria.idDiaria}`}>
-                                                            <i className="fas fa-check"></i>
-                                                        </Link>
-                                                    }
-                                                    
-                                                </td>
-                                                <td>
-                                                    {
-                                                        diaria.dataPgto ?
-                                                        <button className="btn btn-danger" onClick={() => cancelarPagamento(diaria.idDiaria)}>
-                                                            <i className="fas fa-ban"></i>
-                                                        </button>
-                                                        :
-                                                        <button className="btn btn-danger" disabled><i className="fas fa-ban"></i></button>
-                                                    }
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                        :
-                        <div>Este funcionário não possui diárias.</div>
-                    :
-                    <></>
-                }
-                
-            </div>
+                                <tbody>
+                                    {
+                                        
+                                        listaDiarias.map((diaria) => {
+                                            return (
+                                                <tr key={diaria.idDiaria}>
+                                                    <td>{formatarData(diaria.dia)}</td>
+                                                    <td>R$ {parseFloat(diaria.valorDiaria).toFixed(2).replace('.', ',')}</td>
+                                                    <td>
+                                                        {
+                                                            diaria.dataPgto ?
+                                                            formatarData(diaria.dataPgto)
+                                                            :
+                                                            "Não foi paga"
+                                                        }
+                                                    </td>
+                                                    <td>
+                                                        {
+                                                            diaria.dataPgto ?
+                                                            <button disabled className="btn btn-success" href={`diarias/alterar/${diaria.idDiaria}`}>
+                                                                <i className="fas fa-check"></i>
+                                                            </button>
+                                                            :
+                                                            <Link className="btn btn-success" href={`diarias/alterar/${diaria.idDiaria}`}>
+                                                                <i className="fas fa-check"></i>
+                                                            </Link>
+                                                        }
+                                                        
+                                                    </td>
+                                                    <td>
+                                                        {
+                                                            diaria.dataPgto ?
+                                                            <button className="btn btn-danger" onClick={() => cancelarPagamento(diaria.idDiaria)}>
+                                                                <i className="fas fa-ban"></i>
+                                                            </button>
+                                                            :
+                                                            <button className="btn btn-danger" disabled><i className="fas fa-ban"></i></button>
+                                                        }
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                            :
+                            <div>Diárias não encontradas com os critérios especificados.</div>
+                        }
+                    </div>
+                :
+                <></>
+            }
         </div>
     );
 }
